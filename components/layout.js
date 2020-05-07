d869a3abebbe58e2bf6f742b286619b5f2c22b85
Victor Guadarrama <victor.guadarrama3@gmail.com>
2020-05-06 23:34:17 -0500

    update name

--- components/layout.js
+++ components/layout.js
@@ -3,8 +3,8 @@ import styles from "./layout.module.css";
 import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
 
-const name = "Fantomas";
-export const siteTitle = "Next.js Sample Website";
+const name = "Magnifique!";
+export const siteTitle = "Another Next.js Sample Website";
 
 export default function Layout({ children, home }) {
   return (
